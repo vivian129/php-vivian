6fe0df2bd457160d8686d76f76ef2368abe2b544
terryshikanga <teresashikanga68@gmail.com>
2019-02-25 16:43:27 +0300

    vivian

---
+++ errors.php
@@ -0,0 +1,9 @@
+
+<?php if (count($errors)>0) ;?>
+
+    <div class="error">
+
+        <?php foreach($errors as $error); ?>
+        <p><?php  echo $error  ?></p>
+        <?phpendforeach?>
+    </div>
